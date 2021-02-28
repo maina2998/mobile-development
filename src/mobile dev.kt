72f79277f43147ad35ee301f95ede6a7c749b40d
student <mainagracewacheke@gmail.com>
2021-02-28 10:58:41 +0300

    first mobile development

---
+++ src/mobile dev.kt
@@ -0,0 +1,32 @@
+fun main() {
+    var name :String ="Hello Grace"
+    println(name)
+
+    var modulus = (500 % 10)
+    var result = "num1,num2"
+    println(modulus)
+
+    var addition = (400 +200 +300 +600)
+    var resu ="no1,no2,no3,no4"
+    println(addition)
+
+    var short :Boolean =true
+    println(short)
+
+
+}
+fun modulus(num1:Int,num2:Int) : Int{
+    var modulus =num1%num2
+    return(modulus)
+
+
+}
+fun addition(no1:Int,no2:Int,no3:Int,no4:Int) : Int{
+    var addition =no1 + no2 +no3 +no4
+    return(addition)
+
+}
+
+
+
+
